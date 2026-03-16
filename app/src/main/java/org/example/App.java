9eadfa225426794f32b3f9f3942bff9d1cb88485
Nadia M <tm53929@mail.harpercollege.edu>
2026-03-15 21:20:39 -0700

    Recursion Lab

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -4,11 +4,22 @@
 package org.example;
 
 public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
-
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        TriangleNumberCalculator calculator = new TriangleNumberCalculator();
+
+        System.out.println(calculator.value(1));
+        System.out.println(calculator.value(2));
+        System.out.println(calculator.value(4));
+
+        System.out.println(calculator.add(1, 1));
+        System.out.println(calculator.add(2, 3));
+        System.out.println(calculator.add(4, 2));
+        System.out.println(calculator.add(4, 2));
+        System.out.println(calculator.subtract(1, 1));
+        System.out.println(calculator.subtract(2, 5));
+        System.out.println(calculator.subtract(4, 2));
+
+        System.out.println(calculator.multiply(2, 3));
+
     }
 }
